20da6fe144bd3f0dc8325384ba7b94cd9c93d615
katielrink <71799430+katielrink@users.noreply.github.com>
2021-10-26 20:14:33 -0500

    Update GymOwnerHomePage.jsx

--- frontend/src/components/GymOwnerHomePage/GymOwnerHomePage.jsx
+++ frontend/src/components/GymOwnerHomePage/GymOwnerHomePage.jsx
@@ -4,5 +4,37 @@ import './GymOwnerHomePage.css';
 import { API_BASE_URL, ACCESS_TOKEN_NAME } from '../../constants/apiConstants';
 import { withRouter } from "react-router-dom";
 
+<<<<<<< Updated upstream
 function GymOwnerHomePage(props) {}
+=======
+function GymOwnerHomePage(props) {
+    const [state , setState] = useState({
+        username : "",
+        gymnname : "",
+        description : "",
+        amenities : null,
+        PTs : null
+    })
+    const loadData = (e) => {
+        axios.get(API_BASE_URL, { headers: { 'token': localStorage.getItem(ACCESS_TOKEN_NAME) }})
+        .then(function (response) {
+            if(response.status !== 200){
+              redirectToLogin()
+            }
+        })
+        .catch(function (error) {
+          redirectToLogin()
+        });
+    }
+    const redirectToLogin = () => {
+      props.updateTitle('Login')
+      props.history.push('/login'); 
+    }
+    return(
+      <div className="GymPage">
+        
+      </div>
+    )
+}
+>>>>>>> Stashed changes
 export default withRouter(GymOwnerHomePage);
