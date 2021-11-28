3156e1b1f5830f916915bee0b56c6805d3eb0958
williamchan8 <chanw@smu.edu>
2021-11-28 16:10:41 -0600

    Added routing

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,19 +5,16 @@ import Home from './components/Home/Home';
 import GymOwnerHomePage from './components/GymOwnerHomePage/GymOwnerHomePage'
 import UserHomePage from './components/TrainerHomePage/UserHomePage';
 import TrainerHomePage from './components/TrainerHomePage/TrainerHomePage';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import AlertComponent from './components/AlertComponent/AlertComponent'; 
+
 
 export const routes = [
     { path: '/', component: LoginForm},
     { path: '/login', component: LoginForm },
     { path: '/register', component: RegistrationForm },
     { path: '/UserHomePage/:userID' ,exact : true, component: UserHomePage},
-    { path: '/TrainerHomePage/:trainerID' ,exact : true, component: TrainerHomepage},
+    { path: '/TrainerHomePage/:trainerID' ,exact : true, component: TrainerHomePage},
+    { path: '/GymOwnerHomePage/:ownerID' ,exact : true, component: GymOwnerHomePage},
+
 
 
 ];
