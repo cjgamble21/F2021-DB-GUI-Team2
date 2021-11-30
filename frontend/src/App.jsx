e5c6b3393e014e6df341be0ad8d93daae3449fca
cjgamble21 <cjgamble21@gmail.com>
2021-11-30 12:11:15 -0600

    Fixed api urls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,11 +30,11 @@ function App () {
   const [values, setValues] = useState([])
 
   // ENTER YOUR EC2 PUBLIC IP/URL HERE
-  const ec2_url = ''
+  const ec2_url = 'http://ec2-3-139-91-59.us-east-2.compute.amazonaws.com:8000'
   // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
   const ec2 = false;
   // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
-  const url = ec2 ? ec2_url : 'localhost'
+  const url = ec2 ? ec2_url : 'http://localhost:8000'
 
   return (
     <Router>
--- frontend/src/constants/apiConstants.jsx
+++ frontend/src/constants/apiConstants.jsx
@@ -1,2 +1,2 @@
 export var ACCESS_TOKEN_NAME = 'login_access_token';
-export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = 'http://ec2-3-139-91-59.us-east-2.compute.amazonaws.com:8000';
